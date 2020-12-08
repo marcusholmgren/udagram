040f746cc3ad14afbbec9ebd6695da4aae1d179a
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-12-08 20:59:54 +0100

    chore: add Prettier to REST API microservice

--- restapi/src/config/config.ts
+++ restapi/src/config/config.ts
@@ -2,21 +2,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 export const config = {
-  "dev": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": process.env.DIALECT,
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  dev: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: '',
+    password: '',
+    database: 'udagram_prod',
+    host: '',
+    dialect: 'postgres',
+  },
+};
--- restapi/src/controllers/v0/model.index.ts
+++ restapi/src/controllers/v0/model.index.ts
@@ -2,4 +2,4 @@ import { FeedItem } from './feed/models/FeedItem';
 import { User } from './users/models/User';
 
 // ts-ignore
-export const V0MODELS = [ FeedItem, User ];
+export const V0MODELS = [FeedItem, User];
